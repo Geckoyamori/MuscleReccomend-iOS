026cbc970f448b58603f6fce897f76c8cd4b6f24
Geckoyamori <69742111+Geckoyamori@users.noreply.github.com>
2021-08-15 17:39:47 +0900

    initial preparation

---
+++ MuscleReccomend/MuscleReccomend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuscleReccomend
+//
+//  Created by 多喜和弘 on 2021/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuscleReccomend/MuscleReccomend/MuscleReccomendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleReccomendApp.swift
+//  MuscleReccomend
+//
+//  Created by 多喜和弘 on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct MuscleReccomendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
