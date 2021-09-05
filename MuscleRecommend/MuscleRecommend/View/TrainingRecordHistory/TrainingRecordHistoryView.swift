80dd13cbf897e8841c1ab2d3da89257f29ecfbc1
Geckoyamori <69742111+Geckoyamori@users.noreply.github.com>
2021-09-05 23:29:44 +0900

    #5 メニュービューの大枠作成

--- MuscleRecommend/MuscleRecommend/View/TrainingRecordHistory/TrainingRecordHistoryView.swift
+++ MuscleRecommend/MuscleRecommend/View/TrainingRecordHistory/TrainingRecordHistoryView.swift
@@ -14,9 +14,47 @@ struct TrainingRecordHistoryView: View {
     // 筋トレメニューID
     let trainingMenuId: String
     
+    // 初回筋トレメニューViewのレイアウト
+    private let INITIAL_DESCRIPTION = "初回の%@トレーニングを記録後に、\n次回以降の推奨メニューが表示されます。"
+    private let INITIAL_COLOR = #colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5764705882, alpha: 0.3034032534)
+    // 推奨筋トレメニュービューのレイアウト
+    private let RECOMMEND_LAYOUT_ARRAY = [StrengthLayout(strength: "高強度", color: #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 0.3043396832)), StrengthLayout(strength: "中強度", color: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 0.2987478596)), StrengthLayout(strength: "低強度", color: #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 0.3016641695))]
+    // 推奨筋トレメニューViewの表示フラグ
+    @State private var isRecommendedHighStrengthMenuPresented = false
+    
     var body: some View {
-        Text(trainingMenuId)
+        NavigationView {
+            VStack(spacing: 20) {
+                // 推奨筋トレメニューViewの生成
+                ForEach(RECOMMEND_LAYOUT_ARRAY, id: \.self) { strengthLayout in
+                    ZStack {
+                        // 初回筋トレメニューViewの生成
+                        HStack {
+                            Spacer().frame(width: 20)
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(INITIAL_COLOR))
+                                .frame(height: 100)
+                            Spacer().frame(width: 20)
+                        }.overlay( Text(String(format: NSLocalizedString(INITIAL_DESCRIPTION, comment: ""), strengthLayout.strength)))
+                        // 推奨筋トレメニューViewの生成
+                        HStack {
+                            Spacer().frame(width: 20)
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(strengthLayout.color))
+                                .frame(height: 100)
+                            Spacer().frame(width: 20)
+                        }.overlay( Text(String(format: NSLocalizedString(INITIAL_DESCRIPTION, comment: ""), strengthLayout.strength)))
+                    }
+                }
+            }
+        }
     }
+}
+
+// 各強度のレイアウト定義
+struct StrengthLayout: Hashable {
+    let strength: String
+    let color: UIColor
 }
 
 struct TrainingRecordHistoryView_Previews: PreviewProvider {
